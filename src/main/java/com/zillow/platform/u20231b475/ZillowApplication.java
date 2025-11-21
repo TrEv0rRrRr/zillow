710cdf65fc4e40bb09f728dca138e986ee1f8755
Trevor <valentinosolischang@gmail.com>
2025-11-20 19:14:27 -0500

    Add ZillowApplication class with Spring Boot setup

---
+++ src/main/java/com/zillow/platform/u20231b475/ZillowApplication.java
@@ -0,0 +1,13 @@
+package com.zillow.platform.u20231b475;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZillowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZillowApplication.class, args);
+	}
+
+}
