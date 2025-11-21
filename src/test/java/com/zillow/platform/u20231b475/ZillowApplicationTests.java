edebcb875050932532c619dd411cfbc01ad0e271
Trevor <valentinosolischang@gmail.com>
2025-11-20 19:15:28 -0500

    Add ZillowApplicationTests for application context loading verification

---
+++ src/test/java/com/zillow/platform/u20231b475/ZillowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zillow.platform.u20231b475;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZillowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
