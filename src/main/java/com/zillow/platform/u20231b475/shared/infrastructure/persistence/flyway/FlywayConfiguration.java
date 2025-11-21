4f018193136a77faa5d61d43df8397199f192d41
Trevor <valentinosolischang@gmail.com>
2025-11-20 19:15:19 -0500

    Add FlywayConfiguration class for database migration setup

---
+++ src/main/java/com/zillow/platform/u20231b475/shared/infrastructure/persistence/flyway/FlywayConfiguration.java
@@ -0,0 +1,16 @@
+package com.zillow.platform.u20231b475.shared.infrastructure.persistence.flyway;
+
+import org.flywaydb.core.Flyway;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class FlywayConfiguration {
+
+  @Autowired
+  public FlywayConfiguration(DataSource dataSource) {
+    Flyway.configure().baselineOnMigrate(true).dataSource(dataSource).load().migrate();
+  }
+}
