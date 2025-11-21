a458c62995d0ba8cd705de430e590b70630b1040
Trevor <valentinosolischang@gmail.com>
2025-11-21 00:40:10 -0500

    Remove GetLeadByEmailAndSourceQuery class as it is no longer needed

--- src/main/java/com/zillow/platform/u20231b475/crm/domain/model/queries/GetLeadByEmailAndSourceQuery.java
+++
@@ -1,9 +0,0 @@
-package com.zillow.platform.u20231b475.crm.domain.model.queries;
-
-import com.zillow.platform.u20231b475.crm.domain.model.valueobjects.LeadSource;
-
-/**
- * Query to retrieve a lead by its email and source
- */
-public record GetLeadByEmailAndSourceQuery(String email, LeadSource source) {
-}
