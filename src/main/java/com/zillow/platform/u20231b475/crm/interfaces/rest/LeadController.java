66b1b8bea902df8d1ca54208eb762e9ae530b8b0
Trevor <valentinosolischang@gmail.com>
2025-11-21 01:17:16 -0500

    Remove unused import and add missing RequestBody annotation in LeadController

--- src/main/java/com/zillow/platform/u20231b475/crm/interfaces/rest/LeadController.java
+++ src/main/java/com/zillow/platform/u20231b475/crm/interfaces/rest/LeadController.java
@@ -5,6 +5,7 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,7 +16,6 @@ import com.zillow.platform.u20231b475.crm.interfaces.rest.transform.CreateLeadCo
 import com.zillow.platform.u20231b475.crm.interfaces.rest.transform.LeadResourceFromEntityAssembler;
 
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
