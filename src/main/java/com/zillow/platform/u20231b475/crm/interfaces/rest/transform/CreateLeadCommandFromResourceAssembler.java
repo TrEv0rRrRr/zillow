ed6cba2e734e361fed0ef17c28e4c25cb8684797
Trevor <valentinosolischang@gmail.com>
2025-11-21 00:49:42 -0500

    Implement conversion methods in CreateLeadCommandFromResourceAssembler and LeadResourceFromEntityAssembler

--- src/main/java/com/zillow/platform/u20231b475/crm/interfaces/rest/transform/CreateLeadCommandFromResourceAssembler.java
+++ src/main/java/com/zillow/platform/u20231b475/crm/interfaces/rest/transform/CreateLeadCommandFromResourceAssembler.java
@@ -1,5 +1,31 @@
 package com.zillow.platform.u20231b475.crm.interfaces.rest.transform;
 
-public class CreateLeadCommandFromResourceAssembler {
+import com.zillow.platform.u20231b475.crm.domain.model.commands.CreateLeadCommand;
+import com.zillow.platform.u20231b475.crm.interfaces.rest.resources.CreateLeadResource;
 
+/**
+ * Assembler to convert a CreateLeadResource to a CreateLeadCommand.
+ */
+public class CreateLeadCommandFromResourceAssembler {
+  /**
+   * Converts a CreateLeadResource to a CreateLeadCommand
+   * 
+   * @param sellerId The seller Id
+   * @param resource The {@link CreateLeadResource} resource to convert
+   * @return The {@link CreateLeadCommand} command
+   */
+  public static CreateLeadCommand toCommandFromResource(Long sellerId, CreateLeadResource resource) {
+    return new CreateLeadCommand(
+        sellerId,
+        resource.email(),
+        resource.age(),
+        resource.incomeLevel(),
+        resource.fullName(),
+        resource.phone(),
+        resource.locationPreference(),
+        resource.minBedrooms(),
+        resource.amount(),
+        resource.currency(),
+        resource.source());
+  }
 }
--- src/main/java/com/zillow/platform/u20231b475/crm/interfaces/rest/transform/LeadResourceFromEntityAssembler.java
+++ src/main/java/com/zillow/platform/u20231b475/crm/interfaces/rest/transform/LeadResourceFromEntityAssembler.java
@@ -1,5 +1,31 @@
 package com.zillow.platform.u20231b475.crm.interfaces.rest.transform;
 
+import com.zillow.platform.u20231b475.crm.domain.model.aggregates.Lead;
+import com.zillow.platform.u20231b475.crm.interfaces.rest.resources.LeadResource;
+
+/**
+ * Assembler to convert a Lead entity to a LeadResource
+ * 
+ * @author Valentino Solis
+ */
 public class LeadResourceFromEntityAssembler {
 
+  /**
+   * Converts a Lead entity to tan LeadResource
+   * 
+   * @param entity The {@link Lead} entity to convert
+   * @return The {@link LeadResource} resource
+   */
+  public static LeadResource toResourceFromEntity(Lead entity) {
+    return new LeadResource(
+        entity.getId(),
+        entity.getSellerId(),
+        entity.getEmail(),
+        entity.getContactInfo(),
+        entity.getPersonalInfo(),
+        entity.getPurchaseIntent(),
+        entity.getBugdet(),
+        entity.getSource(),
+        entity.getStatus());
+  }
 }
