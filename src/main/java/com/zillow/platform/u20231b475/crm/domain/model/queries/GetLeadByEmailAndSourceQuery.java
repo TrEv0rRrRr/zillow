e36f174fdb0549c0202c1285810b1ac93e1640d8
Trevor <valentinosolischang@gmail.com>
2025-11-21 00:15:01 -0500

    Refactor GetLeadByEmailAndSourceQuery to use String for email instead of EmailAddress

--- src/main/java/com/zillow/platform/u20231b475/crm/domain/model/queries/GetLeadByEmailAndSourceQuery.java
+++ src/main/java/com/zillow/platform/u20231b475/crm/domain/model/queries/GetLeadByEmailAndSourceQuery.java
@@ -1,10 +1,9 @@
 package com.zillow.platform.u20231b475.crm.domain.model.queries;
 
-import com.zillow.platform.u20231b475.crm.domain.model.valueobjects.EmailAddress;
 import com.zillow.platform.u20231b475.crm.domain.model.valueobjects.LeadSource;
 
 /**
  * Query to retrieve a lead by its email and source
  */
-public record GetLeadByEmailAndSourceQuery(EmailAddress email, LeadSource source) {
+public record GetLeadByEmailAndSourceQuery(String email, LeadSource source) {
 }
